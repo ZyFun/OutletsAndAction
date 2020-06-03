1bcffc2e6d326a116d44ac620a6747cf0830e864
Dmitriy Danilin <danilindn@icloud.com>
2020-06-03 14:55:42 +0300

    Initial Commit

---
+++ OutletsAndAction/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OutletsAndAction
+//
+//  Created by Дмитрий Данилин on 03.06.2020.
+//  Copyright © 2020 Дмитрий Данилин. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
